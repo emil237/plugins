0b662789a4112a54b01e5ae4dd7ef6ce81192c2c
emil237 <marianatalaat1@gmail.com>
2025-09-09 12:42:14 +0300

    Add files via upload

--- levi45-freeserver/levi45-freeserver.sh
+++ levi45-freeserver/levi45-freeserver.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 #
+TEMPATH=/tmp
+OPKGINSTALL="opkg install --force-reinstall"
+MY_IPK="enigma2-plugin-extensions-levi45-freeserver_all.ipk"
+MY_DEB="enigma2-plugin-extensions-levi45-freeserver_all.deb"
 PLUGIN_DIR="/usr/lib/enigma2/python/Plugins/Extensions/Levi45FreeServer"
 STATUS_FILE="/var/lib/opkg/status"
 PACKAGE_NAME="enigma2-plugin-extensions-levi45-freeserver"
 PLUGIN_NAME="levi45-freeserver"
-URL="https://github.com/emil237/plugins/raw/refs/heads/main/levi45-freeserver/levi45-freeserver.tar.gz"
-DOWNLOAD_PATH="/var/volatile/tmp/$PLUGIN_NAME.tar.gz"
+URL="https://github.com/emil237/plugins/raw/refs/heads/main/levi45-freeserver"
 
-# 
 if [ -d "$PLUGIN_DIR" ]; then
     echo "> Removing old $PLUGIN_NAME plugin, please wait..."
     rm -rf "$PLUGIN_DIR" > /dev/null 2>&1
@@ -21,77 +23,64 @@ if [ -d "$PLUGIN_DIR" ]; then
     echo "*******************************************"
 fi
 
-# 
 pyv="$(python -V 2>&1)"
 echo "$pyv"
 echo "Checking Dependencies"
 echo ""
-echo "Updating feeds..."
+
 if [ -d /etc/opkg ]; then
     opkg update
-    echo ""
-
     case "$pyv" in
-        *Python\ 3*)
-            echo "Checking python3-requests"
-            if python -c "import requests" 2>/dev/null; then
-                echo "Requests library already installed"
-            else
-                opkg install python3-requests
-            fi
-            ;;
-        *)
-            echo "Checking python-requests"
-            if python -c "import requests" 2>/dev/null; then
-                echo "Requests library already installed"
-            else
-                opkg install python-requests
-            fi
-            ;;
+        *Python\ 3*) opkg install python3-requests ;;
+        *) opkg install python-requests ;;
     esac
-
 else
     apt-get update
-    echo ""
-
     case "$pyv" in
-        *Python\ 3*)
-            echo "Checking python3-requests"
-            if python -c "import requests" 2>/dev/null; then
-                echo "Requests library already installed"
-            else
-                apt-get -y install python3-requests
-            fi
-            ;;
-        *)
-            echo "Checking python-requests"
-            if python -c "import requests" 2>/dev/null; then
-                echo "Requests library already installed"
-            else
-                apt-get -y install python-requests
-            fi
-            ;;
+        *Python\ 3*) apt-get -y install python3-requests ;;
+        *) apt-get -y install python-requests ;;
     esac
 fi
 
-# 
 echo "> Downloading $PLUGIN_NAME package, please wait..."
-sleep 1
-wget -O "$DOWNLOAD_PATH" --no-check-certificate "$URL"
+cd $TEMPATH
+set -e
 
-tar -xzf "$DOWNLOAD_PATH" -C /
-EXTRACT_STATUS=$?
-rm -f "$DOWNLOAD_PATH" > /dev/null 2>&1
-echo ""
-
-if [ "$EXTRACT_STATUS" -eq 0 ]; then
-    echo "> $PLUGIN_NAME package installed successfully"
-    sleep 2
+if which dpkg > /dev/null 2>&1; then
+    wget -q "$URL/$MY_DEB"
+    if dpkg -i --force-overwrite $MY_DEB; then
+        apt-get install -f -y
+        INSTALL_OK=1
+    fi
+    rm -f $MY_DEB
 else
-    echo "> $PLUGIN_NAME package installation failed"
+    wget -q "$URL/$MY_IPK"
+    if $OPKGINSTALL $MY_IPK; then
+        INSTALL_OK=1
+    fi
+    rm -f $MY_IPK
+fi
+
+set +e
+cd ..
+
+if [ "$INSTALL_OK" = "1" ]; then
+    echo ">>>>  SUCCESSFULLY INSTALLED <<<<"
+else
+    echo "!!!! INSTALLATION FAILED !!!!"
+fi
+
+echo "********************************************************************************"
+echo "   UPLOADED BY  >>>>   EMIL_NABIL "   
+sleep 3
+echo ". >>>>         RESTARTING     <<<<"
+echo "**********************************************************************************"
+
+if command -v systemctl > /dev/null 2>&1; then
+    systemctl restart enigma2
+else
+    init 4 && sleep 2 && init 3
 fi
 
 exit 0
-
-
 
